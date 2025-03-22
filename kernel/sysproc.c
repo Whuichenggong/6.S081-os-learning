dabcba2da8b97667fb263a2477d3c660af6d9ebb
Whuichenggong <zhaozhonghe40@gmail.com>
2025-03-22 16:39:11 +0800

    completed lab02  read kernel/kalloc.c && read kernel/proc.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -6,6 +6,10 @@
 #include "memlayout.h"
 #include "spinlock.h"
 #include "proc.h"
+#include "sysinfo.h"
+
+uint64 acquire_freemem();
+uint64 acquire_nproc();
 
 uint64
 sys_exit(void)
@@ -95,3 +99,25 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_sysinfo(void)
+{
+  struct sysinfo info;
+  uint64 addr;
+  struct proc *p = myproc();
+
+
+  info.nproc = acquire_nproc();
+  info.freemem = acquire_freemem();
+  
+  if (argaddr(0,&addr)<0) {
+    return -1;
+  }
+
+  if (copyout(p->pagetable,addr,(char*)&info,sizeof(info))<0) {
+    return -1;
+  }
+    return 0;
+}
+ 

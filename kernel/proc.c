6283d1544f8d839f5aa656d7ed100f5c04fe169b
Whuichenggong <zhaozhonghe40@gmail.com>
2025-04-08 15:37:42 +0800

    fix panic TRAPFRAME->USYSCALL about proc.c

--- kernel/proc.c
+++ kernel/proc.c
@@ -192,7 +192,7 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
-  if(mappages(pagetable, TRAPFRAME, PGSIZE,
+  if(mappages(pagetable, USYSCALL, PGSIZE,
               (uint64)(p->usyscall), PTE_R | PTE_U) < 0){
     uvmunmap(pagetable,USYSCALL,1,0);             
     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
